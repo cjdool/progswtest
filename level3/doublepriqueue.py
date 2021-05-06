f56d23ebef47e19c7d5024653574ff46886c96dc
jj <cjdool@naver.com>
2021-05-06 20:11:54 +0900

    doublepriqueue.py completed

--- level3/doublepriqueue.py
+++ level3/doublepriqueue.py
@@ -22,6 +22,7 @@ def solution(operations):
     return answer
 
 
+
 print(solution(["I 16","D 1"]))
 print(solution(["I 7","I 5","I -5","D -1"]))
 print(solution(["I 16", "I -5643", "D -1", "D 1", "D 1", "I 123", "D -1"]))
